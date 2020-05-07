ab81887acb00a12eab1661a908529abb4a074120
Madhukar Krishnarao <madhukark@gmail.com>
2020-05-06 17:58:38 -0700

    Add a TAG to effected VMs

--- run-operations.py
+++ run-operations.py
@@ -17,12 +17,14 @@ requests.packages.urllib3.disable_warnings()
 
 nsx_ip = "10.114.222.72"
 nsx_user = "admin"
-nsx_pass = 'VMware1!VMware1!'
+nsx_pass = 'myPassword!myPassword!'
 
 vc_ip = "10.114.200.6"
-vc_user = "administrator@madhu.local"
-vc_pass = "VMware1!"
+vc_user = "administrator@vsphere.local"
+vc_pass = "myPassword!"
 
+nsx_tag = "IDS-IOC"
+nsx_scope = "isolate"
 
 def get_obj(content, vimtype, name):
     """
@@ -38,24 +40,52 @@ def get_obj(content, vimtype, name):
     return obj
 
 
+def get(url):
+    response = None
+    try:
+      response = requests.get (url, verify=False, auth=(nsx_user, nsx_pass))
+      response.raise_for_status()
+    except HTTPError as http_err:
+        print ('HTTP error occured: %s' % http_err)
+        sys.exit(2)
+    except Exception as err:
+        print ('Unknown error occured: %s' % err)
+        sys.exit(2)
+    return response
+
+
+def post(url, mydata):
+    response = None
+    try:
+        response = requests.post (base_url, verify=False, auth=(nsx_user, nsx_pass), \
+                              data=mydata, headers={'Content-Type': 'application/json'})
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print ('HTTP error occurred: %s' % http_err)
+        sys.exit(2)
+    except Exception as err:
+        print ('Unknown error occurred: %s' % http_err)
+        sys.exit(2)
+    return response
+
+
+# Get all VMs and its external_id and save
+base_url = "https://" + nsx_ip + "/api/v1/fabric/virtual-machines"
+response = get(base_url)
+vm_content = response.json()
+vm_dict = {}
+vm_list = []
+for result in vm_content['results']:
+    vm_dict[result['display_name']] = result['external_id']
+    vm_list.append(result['display_name'])
+
+# Retrieved affected VMs and process
 base_url = "https://" + nsx_ip + "/api/v1/intrusion-services/affected-vms"
 mydata = '{ "filters": [ { "field_names": "signature_detail.signature_id", "value": "(2010937)" } ] }'
 json_object = json.loads(mydata)
 json_formatted_str = json.dumps(json_object, indent=2)
-
-try:
-    response = requests.post (base_url, verify=False, auth=(nsx_user, nsx_pass), \
-                              data=mydata, headers={'Content-Type': 'application/json'})
-    response.raise_for_status()
-except HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')  # Python 3.6
-except Exception as err:
-    print(f'Other error occurred: {err}')  # Python 3.6
-
-
+response = post(base_url, mydata)
 nsx_content = response.json()
-
-# Connect to vSphere
 
 si = None
 context = context = ssl._create_unverified_context()
@@ -71,11 +101,21 @@ for vm_name in nsx_content['results']:
         print ("Virtual Machine %s doesn't exists" % vm_name)
         sys.exit()
 
+    # Add tag to the VM
+    print ("Adding NSX Tag to VM: %s ..." % vm_name)
+    if (vm_name in vm_list):
+        base_url = "https://" + nsx_ip + "/api/v1/fabric/virtual-machines?action=add_tags"
+        mydata = '{ "external_id": "' + vm_dict[vm_name] + '", \
+                     "tags": [ \
+                       { "tag": "' + nsx_tag + '", "scope": "' + nsx_scope + '" } \
+                     ] } '
+        json_object = json.loads(mydata)
+        json_formatted_str = json.dumps(json_object, indent=2)
+        post (base_url, mydata)
+
     print ("Taking a snapshot of VM: %s ..." % vm_name)
     WaitForTask(vm.CreateSnapshot( "vm_name" + "_IDS_snapshot", "Snapshot by NSX IDS", False, False))
 
     if vm.runtime.powerState == vim.VirtualMachinePowerState.poweredOn:
-        # using time.sleep we just wait until the power off action
-        # is complete. Nothing fancy here.
         print ("Powering off VM: %s ..." % vm_name)
         WaitForTask(vm.PowerOff())
